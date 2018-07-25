7b2a5cfefb14bd8128919784240950f2fbc267b6
KyuubiKeith <tensakyuubi5@gmail.com>
2018-07-25 11:43:48 +0300

    Add files via upload
    deleted insomaniac branch and manually added files to master because of git terminal errors...will clone this to proceed.

---
+++ Artista/The_Styles/JavaScript/HavoxWorx.js
@@ -0,0 +1,58 @@
+/*
+ * Author: KyuubiKeith (BlerdCorps Minimalist Designs)
+ * Version: 1.0
+*/
+
+"use strict";
+$(document).ready(function() {
+
+/*-----------------------------------------------------------------
+Preloader
+-------------------------------------------------------------------*/
+function preLoader(){
+    $("body").animate({opacity: 1}, 
+    t, function() {
+        $(".preLoader .word1").animate({
+            opacity: 1
+        }, 
+        t, function() {
+            $(".preLoader .word2").animate({
+                opacity: 1
+            }, 
+            t, function() {
+                $(".preLoader .word3").animate({
+                    opacity: 1
+                }, 
+                t, function() {
+                    $(".preLoader .word4").animate({
+                        opacity: 1
+                    }, 
+                    t, function() {
+                        $(".preLoader .word5").animate({
+                        opacity: 1
+                    },
+                    t, function() {
+                        $(".preLoader span").animate({
+                            opacity: 0
+                        }, t, function() {
+                            $(".preLoader").animate({
+                                opacity: 0
+                            }, t, function() {
+                                $(".preLoader").css('display', 'none');
+                                $(".content .menu").animate({bottom: 0}, t);
+                            });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+/* Call preLoader */
+var t = 1000;/* Time */
+preLoader();
+
+});
+    
